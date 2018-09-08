3f2b7cfcdc3eefe7d3d8be2d224780d71c144961
hiroi <hiroi_t@fringe81.com>
2018-09-08 20:19:02 +0900

    second commit

---
+++ enquete.rb
@@ -0,0 +1,9 @@
+module Enquete
+	def self.hello
+		puts "Hello!"
+	end
+
+	def self.say(message)
+		puts message
+	end
+end
---
+++ seleniumTest.rb
@@ -0,0 +1,32 @@
+require 'selenium-webdriver'
+require './enquete'
+require 'yaml'
+
+config = YAML.load_file("config.yaml")
+
+mail = config["user"]["mail"]
+pass = config["user"]["pass"]
+
+puts mail
+puts pass
+
+driver = Selenium::WebDriver.for :chrome #使用するブラウザを選択
+
+Enquete.hello
+Enquete.say("test dayo")
+
+
+driver.navigate.to "https://ssl.pc.moppy.jp/login/" #URL指定
+
+driver.find_element(:name => "mail").send_keys('tomokazu.hiroi@gmail.com')
+driver.find_element(:name => "pass").send_keys('seraphic3')
+driver.find_element(:class_name, "a-btn__login").click
+
+driver.navigate.to "http://pc.moppy.jp/research/"
+
+driver.find_element(:class_name, "ga-answerBtn").click
+
+sleep(15)
+
+driver.quit #ブラウザを閉じて終了
+
